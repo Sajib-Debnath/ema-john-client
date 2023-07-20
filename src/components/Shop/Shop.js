9ba13c2e81e42017554a59334da09ccdd9eeb1a3
Sajib Debnath <debnathsajib2024@gmail.com>
2023-07-20 01:50:35 -0700

    1st commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { addToDb, deleteShoppingCart, getStoredCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+
+/* 
+count, : loaded
+perPage (size): 10
+pages: count /perPage
+currentPage (page)
+*/
+
+const Shop = () => {
+    // const { products, count } = useLoaderData();
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+    const [cart, setCart] = useState([]);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+
+    useEffect(() => {
+        const url = `http://localhost:5000/products?page=${page}&size=${size}`;
+        console.log(page, size);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setCount(data.count);
+                setProducts(data.products);
+            })
+    }, [page, size])
+
+    const pages = Math.ceil(count / size);
+
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
+
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        const ids = Object.keys(storedCart); 
+        console.log(ids);
+        fetch('http://localhost:5000/productsByIds', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(ids)
+        })
+        .then(res => res.json())
+        .then(data => {
+            for (const id in storedCart) {
+            const addedProduct = data.find(product => product._id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+
+        })
+        
+    }, [products])
+
+    const handleAddToCart = (selectedProduct) => {
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product._id === selectedProduct._id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product._id !== selectedProduct._id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+
+        setCart(newCart);
+        addToDb(selectedProduct._id);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product._id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart clearCart={clearCart} cart={cart}>
+                    <Link to="/orders">
+                        <button>Review Order</button>
+                    </Link>
+                </Cart>
+            </div>
+            <div className="pagination">
+                <p>Currently selected page: {page} and size: {size}</p>
+                {
+                    [...Array(pages).keys()].map(number => <button
+                        key={number}
+                        className={page === number ? 'selected' : ''}
+                        onClick={() => setPage(number)}
+                    >
+                        {number + 1}
+                    </button>)
+                }
+                <select onChange={event => setSize(event.target.value)}>
+                    <option value="5">5</option>
+                    <option value="10" selected>10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWCeGL30ZcgW4akhbweiPqLYxeuyS_GOQ",
+  authDomain: "ema-john-final-78955.firebaseapp.com",
+  projectId: "ema-john-final-78955",
+  storageBucket: "ema-john-final-78955.appspot.com",
+  messagingSenderId: "1008617295100",
+  appId: "1:1008617295100:web:b0f37cdbb536f7bf9c9b79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
